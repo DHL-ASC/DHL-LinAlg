4ed8eace72f2d0f8d6f19b0989ecf97c083af6bc
erdieee <58039191+erdieee@users.noreply.github.com>
2023-10-20 12:09:26 +0200

    add project details to pyproject.toml + dev dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 from skbuild import setup
-import sys
 
 _cmake_args = []
 
 
-
 setup(
-    name="ASCsoft",
-    version="0.0.2",
-    author="J. Schoeberl",
-    license="MIT",
-    packages=["ASCsoft"],
-    cmake_args = _cmake_args
+    cmake_args=_cmake_args,
 )
