2635dfdfef42881d0f1760a110ebb6e3012393a9
erdieee <58039191+erdieee@users.noreply.github.com>
2023-10-20 19:45:36 +0200

    Update test_matrix.py

--- py_tests/test_matrix.py
+++ py_tests/test_matrix.py
@@ -1,16 +1,18 @@
+import pytest
+
 from ASCsoft.bla import Matrix
 from ASCsoft.bla import Vector
 
 
 def test_matrix_vector_multiplication():
     x = Vector(3)
+    m = Matrix(3, 3)
 
     for i in range(len(x)):
         x[i] = i
-    m = Matrix(3, 3)
     for i, j in zip(range(3), range(3)):
         m[i, j] = i + 2 * j
     res = m * x
-    assert res[0] == 0
-    assert res[1] == 3
-    assert res[2] == 12
+    assert res[0] == pytest.approx(0, 1e-16)
+    assert res[1] == pytest.approx(3, 1e-16)
+    assert res[2] == pytest.approx(12, 1e-16)
