96999cb0c37937e529c80248da2f5dd1960ea308
erdieee <58039191+erdieee@users.noreply.github.com>
2023-10-30 12:15:22 +0100

    test: improve vector tests

--- py_tests/test_vector.py
+++ py_tests/test_vector.py
@@ -1,13 +1,33 @@
+import numpy as np
+
 from dhllinalg.bla import Vector
 
 
-def test_add():
-    x = Vector(3)
-    y = Vector(3)
+def test_vector_slicing():
+    x = Vector(5)
+    x[:] = 1
+    assert np.array_equal(np.asarray(x), np.ones(5))
+    x[1::2] = 2
+    assert x[0] == 1
+    assert x[3] == 2
+
+
+def test_vector_add():
+    x = Vector(5)
+    y = Vector(5)
 
     for i in range(len(x)):
         x[i] = i
     y[:] = 2
 
-    for i in range(len(y)):
-        assert y[i] == 2
+    z = x + y
+    assert np.array_equal(np.asarray(z), np.array([2, 3, 4, 5, 6]))
+
+
+def test_vector_scal_mult():
+    x = Vector(5)
+
+    for i in range(len(x)):
+        x[i] = i
+    z = 2 * x
+    assert np.array_equal(np.asarray(z), np.array([0, 2, 4, 6, 8]))
