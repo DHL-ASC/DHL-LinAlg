27b3420861532c2bfc2f9a2115e5d240146eaa24
Sebastian Hirnschall <sebastian@hirnschall.net>
2023-11-04 16:44:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 _cmake_args = ["-DCMAKE_BUILD_TYPE=Release"]
 
-if 'win32' in sys.platform:
-    _cmake_args += ['-mavx']
+#if 'win32' in sys.platform:
+#    _cmake_args += ['-mavx']
 
 
 
