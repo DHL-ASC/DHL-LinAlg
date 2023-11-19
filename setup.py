5c873f400c80c0b715a6d2415baf02bea7d51f85
Sebastian Hirnschall <sebastian@hirnschall.net>
2023-11-19 12:32:29 +0100

    removed /arch:avx2 arg from this file

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 _cmake_args = ["-DCMAKE_BUILD_TYPE=Release"]
 
-if "win32" in sys.platform:
-    _cmake_args += ["/arch:AVX2"]
-
 
 test_deps = ["pytest", "numpy"]
 docs = ["sphinx", "myst-nb", "pandocfilters"]
